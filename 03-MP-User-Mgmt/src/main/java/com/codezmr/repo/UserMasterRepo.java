7d8c069f168c0955482c3007ffc49817f900d4b0
codezmr <mdzamiruddin.zmr@gmail.com>
2022-09-13 14:04:11 +0530

    Repository class

---
+++ 03-MP-User-Mgmt/src/main/java/com/codezmr/repo/UserMasterRepo.java
@@ -0,0 +1,9 @@
+package com.codezmr.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codezmr.entity.UserMaster;
+
+public interface UserMasterRepo extends JpaRepository<UserMaster, Integer>{
+
+}
