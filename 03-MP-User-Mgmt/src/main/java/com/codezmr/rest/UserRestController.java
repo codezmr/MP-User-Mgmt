4c7e77d1e4db2652fd2c63168aba174891fa2e70
codezmr <mdzamiruddin.zmr@gmail.com>
2022-09-21 17:43:50 +0530

    Rest- Delete User Account

--- 03-MP-User-Mgmt/src/main/java/com/codezmr/rest/UserRestController.java
+++ 03-MP-User-Mgmt/src/main/java/com/codezmr/rest/UserRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -69,4 +70,22 @@ public class UserRestController {
 		return new ResponseEntity<User>(user, HttpStatus.OK);
 		
 	}
+	
+	@DeleteMapping("/user/{userId}")
+	public ResponseEntity<String> deleteUserById(@PathVariable Integer userId){
+		
+		boolean isDeleted = service.deleteUserById(userId);
+		
+		if (isDeleted) {
+			String successMsg = "User Account Deleted Success.";
+			return new ResponseEntity<String>(successMsg, HttpStatus.OK);
+		} else {
+			String failureMsg = "Oops!! User Account Deletion Failed.";
+			return new ResponseEntity<String>(failureMsg, HttpStatus.INTERNAL_SERVER_ERROR);
+
+		}
+		
+	}
+	
+	
 }
