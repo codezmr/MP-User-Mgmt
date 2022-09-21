f3ca0e449aed7a86c3402746bcf95302cb387340
codezmr <mdzamiruddin.zmr@gmail.com>
2022-09-21 17:38:36 +0530

    Rest- Get user By Id

--- 03-MP-User-Mgmt/src/main/java/com/codezmr/rest/UserRestController.java
+++ 03-MP-User-Mgmt/src/main/java/com/codezmr/rest/UserRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -60,5 +61,12 @@ public class UserRestController {
 		return new ResponseEntity<List<User>>(allUsers, HttpStatus.OK);
 
 	}
-
+	
+	@GetMapping("/user/{userId}")
+	public ResponseEntity<User> getUserById(@PathVariable Integer userId){
+		
+		User user = service.getUserById(userId);
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+		
+	}
 }
